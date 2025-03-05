1a052efaf447279864da34ceb550e283453c5ffd
Methupa Perera <methupapereradev@gmail.com>
2025-03-05 09:11:29 +0530

    Build error fix.

--- src/components/canvas-reveal-effect.tsx
+++ src/components/canvas-reveal-effect.tsx
@@ -192,7 +192,7 @@ const ShaderMaterial = ({
   uniforms: Uniforms;
 }) => {
   const { size } = useThree();
-  // @ts-expect-error
+  // @ts-ignore
   const ref = useRef<THREE.Mesh>();
   let lastFrameTime = 0;
 
